e3169aab20d41011f7fb4855875e9e694f1c05b0
云岚 <a82886569@gmail.com>
2024-10-26 14:05:45 +0800

    Site updated: 2024-10-26 14:05:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/9ae68f8a/","posts/d50a/","posts/9ae68a89/","posts/9ae68f8s/","posts/9qe48f89/","posts/9ae68f81/","posts/9ae28f81/","posts/9qe68f89/"];function toRandomPost(){
+var posts=["posts/9ae68f8a/","posts/9ae68a89/","posts/9ae68f81/","posts/9ae28f81/","posts/d50a/","posts/9qe48f89/","posts/9ae68f8s/","posts/9qe68f89/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
