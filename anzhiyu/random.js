d34a9b375614adc2e54514dd5f3cf90248f4cae2
yunstarr <a82886569@gmail.com>
2024-10-26 13:51:10 +0800

    Site updated: 2024-10-26 13:51:10

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/d87f7e0c/","posts/33483/","posts/15edad24/","posts/d50a/","posts/d50a/"];function toRandomPost(){
+var posts=["posts/9ae68f8a/","posts/d50a/","posts/9ae68a89/","posts/9ae68f8s/","posts/9qe48f89/","posts/9ae68f81/","posts/9ae28f81/","posts/9qe68f89/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
